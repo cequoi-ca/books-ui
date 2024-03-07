08129d97c8e189fad81f6bf9350afe7cd6e57fa7
Lee-Orr <lee-orr@users.noreply.github.com>
2024-03-07 16:18:43 +0000

    update install

--- devcontainer-features/mcmasterful-books/install.sh
+++ devcontainer-features/mcmasterful-books/install.sh
@@ -5,6 +5,12 @@ cd /
 
 git clone https://github.com/McMastercce/bvd-103-mcmasterful-books
 
+sudo chown -R node:node /bvd-103-mcmasterful-books/
+
+cd /bvd-103-mcmasterful-books
+
+npm install
+
 echo "MCMASTERFUL_ASSIGNMENT=\"$assignment\"" >> /etc/environment
 
 npm install -g ./bvd-103-mcmasterful-books
